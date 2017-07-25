01c8bd9badcb7ff75ef6dde3c52715b5b4d86bb5
William Dussault <dalloriam@gmail.com>
2017-07-25 11:23:20 -0400

    remove useless validation

--- opfront/user.py
+++ opfront/user.py
@@ -3,11 +3,6 @@ from opfront.resource import OpfrontResource
 
 class UserResource(OpfrontResource):
 
-    def update(self, res):
-        if hasattr(res, 'password') and not res.password:
-            del res.password
-        return super().update(res)
-
     def me(self):
         res_url = '{0}/me'.format(self._endpoint)
         body = self._client.do_request(res_url, 'GET')
